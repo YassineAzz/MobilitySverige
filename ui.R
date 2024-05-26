c2e412509d54b5eaf5ac994feb63f7edc35053fc
Yassine Azzimani <134744172+YassineAzz@users.noreply.github.com>
2024-05-26 19:46:35 +0200

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,13 +1,13 @@
-library(shiny)
-library(leaflet)
-library(shinyjs)
-
 ui <- fluidPage(
   useShinyjs(),
-  titlePanel("MobilitySverige"),
+  tags$head(
+    tags$link(rel = "icon", type = "image/x-icon", href = "favicon.png") #icon tab maybe change ????
+  ),
+  titlePanel("MobilityCompare"),
   sidebarLayout(
     sidebarPanel(
       width = 4,
+      img(src = "logo.png", height = "100px", style = "display: block; margin: 0 auto;"), # logo okok
       selectInput(
         "Country", 
         "Country:", 
@@ -21,20 +21,22 @@ ui <- fluidPage(
       ),
       actionButton("go", "Search", class = "btn-primary"),
       hr(),
-      h4("City Informations"),
+      h4("City Information"),
       div(
         style = "margin-top: 10px; padding: 10px; border: 1px solid #ddd; border-radius: 4px; background-color: #f9f9f9;",
         strong(textOutput("cityPopulation")),
         strong(textOutput("cityArea")),
         strong(textOutput("cityDensity"))
       ),
-      h4("Mobility Informations"),
+      h4("Mobility Information"),
       div(
         style = "margin-top: 10px; padding: 10px; border: 1px solid #ddd; border-radius: 4px; background-color: #f9f9f9;",
         strong(textOutput("totalRecords")),
         strong(textOutput("densityStopStation")),
-      )
-    ),
+      ),
+      hr(),
+      div(DTOutput("poiTable"))
+      ),
     mainPanel(
       leafletOutput("mapPlot", height = 600)
     )
